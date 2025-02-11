1f0173e7d65403649a34a9c7d0020a388ec85f42
Eran Assraf <maghrebijalebi@gmail.com>
2025-02-11 17:14:35 +0200

    Add configuration files

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export interface Service {
+  id: string;
+  title: string;
+  description: string;
+  examples: string[];
+  icon: any;
+}
+
+export interface AutomationPlatform {
+  id: string;
+  name: string;
+  description: string;
+  features: string[];
+  icon: string;
+} 
